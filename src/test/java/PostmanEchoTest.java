b816af85c4b1d3af4cb17482e89f74d8238b2d4b
JP <email>
2023-01-09 22:51:06 +1000

    test failed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("correct data"))
+                .body("data", equalTo("wrong data"))
         ;
     }
 }
